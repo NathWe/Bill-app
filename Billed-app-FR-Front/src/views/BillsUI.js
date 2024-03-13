72e2ac14261ca30b6a7a5df8d396e13b448406e2
Nathalie Mallet <nathalie.nathweb@gmail.com>
2024-03-13 09:02:53 +0100

    correction bug 1 et 2

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -17,14 +17,22 @@ const row = (bill) => {
       </td>
     </tr>
     `)
-  }
-
-const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
+const rows = (data) => {
+  if (data) {
+    return data
+      .sort((bill1, bill2) => {
+        return new Date(bill2.date) - new Date(bill1.date);
+      })
+      .map((bill) => row(bill))
+      .join("");
+  } else {
+    return "";
+  }
+};
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +55,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
